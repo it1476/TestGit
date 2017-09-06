17fd183d36e2be6221e17acc35396cac8884abb6
George <it1476@uom.edu.gr>
2017-09-06 15:58:55 +0300

    listview add

--- app/src/main/java/com/example/george/testgit/MainActivity.java
+++ app/src/main/java/com/example/george/testgit/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.george.testgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.List;
+
+import static java.lang.Boolean.TRUE;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView list = (ListView) findViewById(R.id.List);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list.setClickable(TRUE);
     }
+
 
     //test test test
 }
